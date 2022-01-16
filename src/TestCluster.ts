64f0bf3afde8847a8f3907959346f4b5528b4441
Jeffrey Zhang <jeffrey.zhang@calix.com>
2022-01-16 16:26:14 +0800

    make TestCluster pass compile

--- src/TestCluster.ts
+++ src/TestCluster.ts
@@ -1,15 +1,16 @@
-import cluster from 'cluster';
+import * as cluster from 'cluster';
 import { cpus } from 'os';
 const numCPUs = cpus().length;
-
-if (cluster.isPrimary) {
+// @ts-ignore: Unreachable code error
+if (cluster.isMaster) {
   console.log(`Primary ${process.pid} is running`);
 
   // Fork workers.
   for (let i = 0; i < numCPUs; i++) {
+     // @ts-ignore: Unreachable code error
     cluster.fork();
   }
-
+ // @ts-ignore: Unreachable code error
   cluster.on('exit', (worker, code, signal) => {
     console.log(`worker ${worker.process.pid} died`);
   });
